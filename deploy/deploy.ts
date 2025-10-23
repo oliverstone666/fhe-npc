acbda7ac58b3121b68b0b3f16ee0bf4f313534b6
oliverstone666 <RackiDinger12071993oz@hotmail.com>
2025-10-23 15:41:52 +0800

    update deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,17 +1,55 @@
+import * as dotenv from "dotenv";
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
+dotenv.config();
+
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
-  const { deploy } = hre.deployments;
+  const { deploy, log } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  if (hre.network.name === "hardhat" || hre.network.name === "localhost") {
+    if (hre.fhevm) {
+      await hre.fhevm.initializeCLIApi();
+    }
+  }
+
+  log(`Deployer address: ${deployer}`);
+
+  const confidentialGameCoin = await deploy("ConfidentialGameCoin", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  const gameItemNFT = await deploy("GameItemNFT", {
+    from: deployer,
+    log: true,
+    args: ["https://zama.quest/items/"],
+  });
+
+  const isSepolia = hre.network.name === "sepolia";
+  const sepoliaOracle = "0xa02Cda4Ca3a71D7C46997716F4283aa851C28812";
+  const oracleAddress = isSepolia ? sepoliaOracle : hre.ethers.ZeroAddress;
+  const enforceOracle = isSepolia;
+
+  const zamaQuest = await deploy("ZamaQuest", {
+    from: deployer,
+    log: true,
+    args: [
+      confidentialGameCoin.address,
+      gameItemNFT.address,
+      "https://zama.quest/rewards/",
+      "Cipher Blade",
+      oracleAddress,
+      enforceOracle,
+    ],
+  });
+
+  log(`ConfidentialGameCoin deployed at ${confidentialGameCoin.address}`);
+  log(`GameItemNFT deployed at ${gameItemNFT.address}`);
+  log(`ZamaQuest deployed at ${zamaQuest.address}`);
 };
+
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_zama_quest";
+func.tags = ["ZamaQuest"];
